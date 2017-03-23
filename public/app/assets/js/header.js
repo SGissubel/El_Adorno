70c781bf6bec71f07c8ec0fdb45d767566e0c17b
Vincent M. Visconti <vviscont@SPF.local>
2017-03-23 15:47:48 -0400

    worked on footer and header transitions and animations.

---
+++ public/app/assets/js/header.js
@@ -0,0 +1,24 @@
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 50) {
+    if ($('.navbar-brand').hasClass("byname-lg")) {
+      $('.navbar-brand').removeClass("byname-lg").removeClass("fade-in").addClass("byname-sm").addClass("fade-out");
+      $('.navbar').removeClass("navbar-lg").addClass("navbar-sm");
+    }
+  } else {
+    if ($('.navbar-brand').hasClass("byname-sm")) {
+      $('.navbar-brand').removeClass("byname-sm").removeClass("fade-in").addClass("byname-lg").addClass("fade-out");
+      $('.navbar').removeClass("navbar-sm").addClass("navbar-lg");
+    }
+  }
+});
+
+$('.navbar').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+  function (e) {
+    if ($('.navbar-brand').hasClass("byname-lg")) {
+      $('.byname').html('Dream... Create... Design... your inspirations with <span class="logo-font">MIΛ VETRINΛ</span> by impulso');
+    } else {
+      $('.byname').html('<span class="logo-font">MIΛ VETRINΛ</span> by impulso');
+    }
+    $('.navbar-brand').removeClass("fade-out");
+    $('.navbar-brand').addClass("fade-in");
+  });
